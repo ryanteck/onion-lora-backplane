93d591e7d6978fc89880369640845202cddbeafd
Ryan Walmsley <ryanteck@gmail.com>
2018-03-14 19:53:22 +0000

    Try

---
+++ libloragw/inc/config.h
@@ -0,0 +1,13 @@
+#ifndef _LORAGW_CONFIGURATION_H
+#define _LORAGW_CONFIGURATION_H
+	#define LIBLORAGW_VERSION	"3.1.0"
+	#define CFG_SPI_NATIVE	1
+	#define DEBUG_AUX	0
+	#define DEBUG_SPI	0
+	#define DEBUG_REG	0
+	#define DEBUG_HAL	0
+	#define DEBUG_GPS	0
+	#define DEBUG_GPIO	
+	#define CFG_SPI_HALF_DUPLEX	1
+	#include "mtk.h"
+#endif
---
+++ libloragw/inc/mtk.h
@@ -0,0 +1,19 @@
+/*
+ * mtk.h
+ *
+ *  Created on: Sep 25, 2017
+ *      Author: benjaminmarty
+ */
+
+#ifndef _MTK_H_
+#define _MTK_H_
+
+/* Human readable platform definition */
+#define DISPLAY_PLATFORM "MTK"
+
+/* parameters for native spi */
+#define SPI_SPEED		8000000
+#define SPI_DEV_PATH	"/dev/spidev32766.1"
+#define SPI_CS_CHANGE   0
+
+#endif /* _MTK_H_ */
